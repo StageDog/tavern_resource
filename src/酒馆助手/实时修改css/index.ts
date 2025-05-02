be478ccd69d36debe696ca52f348f3e73f096a67
StageDog <aksanajw845@gmail.com>
2025-05-03 01:18:45 +0800

    [酒馆助手/实时修改css] 迁移

---
+++ src/酒馆助手/实时修改css/index.ts
@@ -0,0 +1,25 @@
+const css_url = '填入你的css链接';
+const delay = 1000;
+
+let css: string;
+async function refresh_css() {
+  const response = await fetch(css_url);
+  if (!response.ok) {
+    toastr.error(`未能从 '${css_url}' 获取 css 文件, 请确认链接是否有效`, '实时修改css');
+    return;
+  }
+
+  const new_css = await response.text();
+  if (css != new_css) {
+    css = new_css;
+    $('#customCSS').val(css)[0].dispatchEvent(new Event('input'));
+  }
+}
+
+let id: number;
+$(() => {
+  id = setInterval(refresh_css, delay);
+});
+$(window).on('unload', () => {
+  clearInterval(id);
+});
