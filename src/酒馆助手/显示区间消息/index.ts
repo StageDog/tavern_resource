e4e4632b97d23d67cb07d92b6a8a75ad8e092a19
StageDog <aksanajw845@gmail.com>
2025-05-11 17:37:35 +0800

    [酒馆助手/显示区间消息] 初版

---
+++ src/酒馆助手/显示区间消息/index.ts
@@ -0,0 +1,29 @@
+$(() => {
+  eventOnButton('显示区间', async () => {
+    const min = 0;
+    const max = SillyTavern.chat.length - 1;
+
+    const range = (await SillyTavern.callGenericPopup(
+      `请输入显示区间 (格式: 0-10, 范围: ${min}-${max})`,
+      SillyTavern.POPUP_TYPE.INPUT,
+    )) as string | undefined;
+    if (!range) {
+      toastr.error('未填入有效区间');
+      return;
+    }
+    const [start, end] = range.split('-').map(Number);
+    if (start < min || start > end || end > max) {
+      toastr.error('未填入有效区间');
+      return;
+    }
+    $('#chat').children().remove();
+    for (let i = start; i <= end; i++) {
+      builtin.addOneMessage(SillyTavern.chat[i], { forceId: i });
+      eventEmit(
+        SillyTavern.chat[i].is_user ? tavern_events.USER_MESSAGE_RENDERED : tavern_events.CHARACTER_MESSAGE_RENDERED,
+        i,
+      );
+    }
+  });
+  eventOnButton('恢复默认', SillyTavern.reloadCurrentChat);
+});
