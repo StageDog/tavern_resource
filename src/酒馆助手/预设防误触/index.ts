c9b8a6bb603b1b41ec40a2a60189bcf345512397
StageDog <aksanajw845@gmail.com>
2025-08-09 14:52:03 +0800

    [酒馆助手/预设防误触] feat: 最大化预设上下文长度

--- src/酒馆助手/预设防误触/index.ts
+++ src/酒馆助手/预设防误触/index.ts
@@ -1,5 +1,18 @@
 export {};
 
+function unlock_token_length() {
+  const MAX_CONTEXT = 2000000;
+  const settings = SillyTavern.chatCompletionSettings;
+  if (settings.max_context_unlocked === true && settings.openai_max_context === MAX_CONTEXT) {
+    return;
+  }
+
+  $('#oai_max_context_unlocked').prop('checked', true).trigger('input');
+  $('#openai_max_context_counter').val(MAX_CONTEXT);
+  $('#openai_max_context').val(MAX_CONTEXT).trigger('input');
+}
+const unlock_token_length_debounced = _.debounce(unlock_token_length, 1000);
+
 function lock_inputs(enable: boolean) {
   $('#range_block_openai :input').prop('disabled', enable);
   $('#openai_settings > div:first-child :input').prop('disabled', enable);
@@ -8,6 +21,7 @@ function lock_inputs(enable: boolean) {
 }
 
 $(() => {
+  eventOn(tavern_events.SETTINGS_UPDATED, unlock_token_length_debounced);
   lock_inputs(true);
 });
 
