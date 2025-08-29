eedff282b503a720b495a242f42c6daa2f548934
StageDog <aksanajw845@gmail.com>
2025-08-29 20:38:56 +0800

    [酒馆助手/压缩相邻消息] fix: 关闭时 unmount

--- src/酒馆助手/压缩相邻消息/panel.ts
+++ src/酒馆助手/压缩相邻消息/panel.ts
@@ -2,11 +2,14 @@ import panel from './panel.vue';
 
 import { createApp } from 'vue';
 
+const app = createApp(panel);
+
 export function init_panel() {
   $('#extensions_settings2').append(`<div id="squash_nearby_same_role_messages_settings"></div>`);
-  createApp(panel).mount($('#squash_nearby_same_role_messages_settings')[0]);
+  app.mount($('#squash_nearby_same_role_messages_settings')[0]);
 }
 
 export function destroy_panel() {
   $('#extensions_settings2').find('#squash_nearby_same_role_messages_settings').remove();
+  app.unmount();
 }
