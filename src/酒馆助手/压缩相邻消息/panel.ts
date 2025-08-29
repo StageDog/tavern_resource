aaa3bb9b7e1c0de6584806948083fdaf32731719
StageDog <aksanajw845@gmail.com>
2025-08-29 20:01:03 +0800

    typo

--- src/酒馆助手/压缩相邻消息/panel.ts
+++ src/酒馆助手/压缩相邻消息/panel.ts
@@ -1,6 +1,7 @@
 import panel from './panel.vue';
 
 import { createApp } from 'vue';
+
 export function init_panel() {
   $('#extensions_settings2').append(`<div id="squash_nearby_same_role_messages_settings"></div>`);
   createApp(panel).mount($('#squash_nearby_same_role_messages_settings')[0]);
