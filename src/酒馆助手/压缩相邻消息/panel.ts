f8792625477ee7533a73a6726275b046c87cc1fb
StageDog <aksanajw845@gmail.com>
2025-08-31 12:30:08 +0800

    [酒馆助手/压缩相邻消息] typo

--- src/酒馆助手/压缩相邻消息/panel.ts
+++ src/酒馆助手/压缩相邻消息/panel.ts
@@ -4,7 +4,7 @@ import panel from './panel.vue';
 import { createApp } from 'vue';
 
 const app = createApp(panel);
-const $app = $('<div>').attr('id', 'input_helper');
+const $app = $('<div>').attr('id', 'suqash_adjacent_messages');
 
 export function init_panel() {
   $('#extensions_settings2').append($app);
