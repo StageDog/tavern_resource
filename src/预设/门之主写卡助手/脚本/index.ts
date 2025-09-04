e1f34af397633df3922a6e7816dae05404fadbd0
StageDog <aksanajw845@gmail.com>
2025-09-05 01:03:03 +0800

    [预设/门之主写卡助手] 按钮脚本不检查插件

--- src/预设/门之主写卡助手/脚本/index.ts
+++ src/预设/门之主写卡助手/脚本/index.ts
@@ -1,15 +1,8 @@
 // @no-ci
-import { check_and_install_extensions } from '../../../酒馆助手/自动安装插件/check_and_install_extensions';
 import { destory_buttons, init_buttons } from './buttons';
 import { destory_extension_settings, init_extension_settings } from './extension';
 
 $(() => {
-  check_and_install_extensions([
-    {
-      name: '提示词模板',
-      url: 'https://codeberg.org/zonde306/ST-Prompt-Template',
-    },
-  ]);
   init_extension_settings();
   init_buttons();
 });
