4663ecf997f778c0738d81489affb6511b293900
StageDog <aksanajw845@gmail.com>
2025-09-07 11:53:15 +0800

    [预设/门之主写卡助手] fix: 没安装提示词模板时无法使用的问题

--- src/预设/门之主写卡助手/脚本/extension.ts
+++ src/预设/门之主写卡助手/脚本/extension.ts
@@ -1,14 +1,17 @@
 import { preset_name } from './settings';
 
 function toggle_extension_settings(enable: boolean) {
-  if ($('#macro-replace-disable-toggle').prop('checked') !== !enable) {
-    $('#macro-replace-disable-toggle')
+  const $macro_replace_disable_toggle = $('#macro-replace-disable-toggle');
+  if ($macro_replace_disable_toggle.length > 0 && $macro_replace_disable_toggle.prop('checked') !== !enable) {
+    $macro_replace_disable_toggle
       .prop('checked', !enable)
       .prop('disabled', !enable)[0]
       .dispatchEvent(new Event('click'));
   }
-  if ($('#pt_enabled').prop('checked') !== enable) {
-    $('#pt_enabled').prop('checked', enable).prop('disabled', !enable)[0].dispatchEvent(new Event('click'));
+
+  const $pt_enabled = $('#pt_enabled');
+  if ($pt_enabled.length > 0 && $pt_enabled.prop('checked') !== enable) {
+    $pt_enabled.prop('checked', enable).prop('disabled', !enable)[0].dispatchEvent(new Event('click'));
   }
 }
 
