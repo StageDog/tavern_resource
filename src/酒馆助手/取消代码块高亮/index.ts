ac9d1e936a1d56f69d1088a23868b85d9b3b2b3a
StageDog <aksanajw845@gmail.com>
2025-09-11 18:49:24 +0800

    [酒馆助手/取消代码块高亮] 初版

---
+++ src/酒馆助手/取消代码块高亮/index.ts
@@ -0,0 +1,24 @@
+import { load_readme } from '../../util';
+
+// @ts-expect-error
+const hljs = window.parent.hljs;
+
+const original_highlightElement = hljs.highlightElement;
+
+$(() => {
+  load_readme('https://testingcf.jsdelivr.net/gh/StageDog/tavern_resource/src/酒馆助手/取消代码块高亮/README.md');
+
+  hljs.highlightElement = () => {};
+  $('pre code')
+    .removeAttr('data-highlighted')
+    .text(function () {
+      return $(this).text();
+    });
+});
+
+$(window).on('pagehide', () => {
+  hljs.highlightElement = original_highlightElement;
+  $('pre code').each((_index, element) => {
+    hljs.highlightElement(element);
+  });
+});
