1289b05cba44ecbf2c267eb5e780a765fa997eab
StageDog <aksanajw845@gmail.com>
2025-10-22 18:58:20 +0800

    [禁用酒馆助手宏和提示词模板] fix: 兼容 4.0

--- src/酒馆助手/禁用酒馆助手宏和提示词模板/toggle.ts
+++ src/酒馆助手/禁用酒馆助手宏和提示词模板/toggle.ts
@@ -2,20 +2,17 @@ export function toggleExtensionSettings(enable: boolean) {
   // 酒馆助手 3.0
   const $macro_replace_disable_toggle = $('#macro-replace-disable-toggle');
   if ($macro_replace_disable_toggle.length > 0 && $macro_replace_disable_toggle.prop('checked') !== !enable) {
-    $macro_replace_disable_toggle
-      .prop('checked', !enable)
-      .prop('disabled', !enable)[0]
-      .dispatchEvent(new Event('click'));
+    $macro_replace_disable_toggle.prop('checked', !enable)[0].dispatchEvent(new Event('click'));
   }
 
   // 酒馆助手 4.0
-  const $TH_macro_enabled = $('#TH-macro-enabled');
-  if ($TH_macro_enabled.length > 0 && $TH_macro_enabled.prop('checked') !== !enable) {
-    $TH_macro_enabled.trigger('click').prop('disabled', !enable);
+  const $TH_macro_disabled = $('#TH-macro-enabled, #TH-macro-disabled');
+  if ($TH_macro_disabled.length > 0 && $TH_macro_disabled.prop('checked') !== !enable) {
+    $TH_macro_disabled.trigger('click');
   }
 
   const $pt_enabled = $('#pt_enabled');
   if ($pt_enabled.length > 0 && $pt_enabled.prop('checked') !== enable) {
-    $pt_enabled.prop('checked', enable).prop('disabled', !enable)[0].dispatchEvent(new Event('click'));
+    $pt_enabled.prop('checked', enable)[0].dispatchEvent(new Event('click'));
   }
 }
