708e0ea1fe75f6a90e24036d73920e163e3c3f8c
StageDog <aksanajw845@gmail.com>
2025-10-22 18:35:16 +0800

    [禁用酒馆助手宏和提示词模板] fix: 兼容 4.0

--- src/酒馆助手/禁用酒馆助手宏和提示词模板/toggle.ts
+++ src/酒馆助手/禁用酒馆助手宏和提示词模板/toggle.ts
@@ -1,10 +1,17 @@
 export function toggleExtensionSettings(enable: boolean) {
-  const $macro_replace_disable_toggle = $('#macro-replace-disable-toggle, #TH-macro-enabled');
+  // 酒馆助手 3.0
+  const $macro_replace_disable_toggle = $('#macro-replace-disable-toggle');
   if ($macro_replace_disable_toggle.length > 0 && $macro_replace_disable_toggle.prop('checked') !== !enable) {
     $macro_replace_disable_toggle
       .prop('checked', !enable)
       .prop('disabled', !enable)[0]
       .dispatchEvent(new Event('click'));
+  }
+
+  // 酒馆助手 4.0
+  const $TH_macro_enabled = $('#TH-macro-enabled');
+  if ($TH_macro_enabled.length > 0 && $TH_macro_enabled.prop('checked') !== !enable) {
+    $TH_macro_enabled.trigger('click').prop('disabled', !enable);
   }
 
   const $pt_enabled = $('#pt_enabled');
