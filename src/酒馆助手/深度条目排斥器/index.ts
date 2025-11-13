22f2f94a32a890af0c9d7a24f5ff8c048ee736af
StageDog <aksanajw845@gmail.com>
2025-11-13 15:30:56 +0800

    [酒馆助手/深度条目排斥器] 初版

---
+++ src/酒馆助手/深度条目排斥器/index.ts
@@ -0,0 +1,20 @@
+import { checkMinimumVersion, loadReadme } from 'util/tavern';
+import { Settings } from '酒馆助手/压缩相邻消息/settings';
+import { initSquash } from '酒馆助手/压缩相邻消息/squash';
+
+$(() => {
+  checkMinimumVersion('3.4.17', '深度条目排斥器');
+  loadReadme('https://testingcf.jsdelivr.net/gh/StageDog/tavern_resource/src/酒馆助手/深度条目排斥器/README.md');
+  const { destroy } = initSquash(
+    Settings.parse({
+      name: '深度排斥器',
+      put_system_injection_after_chat_history: true,
+      on_chat_history: {
+        type: 'mixin',
+      },
+    }),
+  );
+  $(window).on('pagehide', () => {
+    destroy();
+  });
+});
