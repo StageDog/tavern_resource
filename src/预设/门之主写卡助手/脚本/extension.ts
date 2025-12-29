f50648abf25d6e9f4d6b5b6a95e893d050a95f1d
StageDog <aksanajw845@gmail.com>
2025-12-29 21:58:55 +0800

    [预设/门之主写卡助手] feat: 调整提示词模板设置

--- src/预设/门之主写卡助手/脚本/extension.ts
+++ src/预设/门之主写卡助手/脚本/extension.ts
@@ -9,12 +9,40 @@ function checkExtensionSettings() {
 }
 const checkExtensionSettingsThrottled = _.throttle(checkExtensionSettings, 1000, { trailing: false });
 
+let settings: EjsTemplate.Features;
+
 export function initExtensionSettings() {
   eventOn(tavern_events.SETTINGS_UPDATED, checkExtensionSettingsThrottled);
   checkExtensionSettings();
+  if (typeof EjsTemplate.getFeatures === 'function') {
+    settings = EjsTemplate.getFeatures();
+    EjsTemplate.setFeatures({
+      enabled: true,
+
+      generate_enabled: true,
+      generate_loader_enabled: true,
+      inject_loader_enabled: true,
+
+      render_enabled: false,
+      render_loader_enabled: false,
+      code_blocks_enabled: false,
+      raw_message_evaluation_enabled: false,
+      filter_message_enabled: false,
+      depth_limit: -1,
+
+      autosave_enabled: false,
+      preload_worldinfo_enabled: false,
+      with_context_disabled: false,
+      debug_enabled: false,
+      invert_enabled: true,
+    });
+  }
 }
 
 export function destroyExtensionSettings() {
   eventRemoveListener(tavern_events.SETTINGS_UPDATED, checkExtensionSettingsThrottled);
   toggleExtensionSettings(true);
+  if (settings) {
+    EjsTemplate.setFeatures(settings);
+  }
 }
