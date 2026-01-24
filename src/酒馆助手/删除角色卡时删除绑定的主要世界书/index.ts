3517c1a11b98adb1fceffe72210abbf4a7d79e54
StageDog <aksanajw845@gmail.com>
2026-01-24 19:08:49 +0800

    [删除角色卡时删除绑定的主要世界书] fix

--- src/酒馆助手/删除角色卡时删除绑定的主要世界书/index.ts
+++ src/酒馆助手/删除角色卡时删除绑定的主要世界书/index.ts
@@ -1,6 +1,8 @@
 $(() => {
   eventOn(tavern_events.CHARACTER_DELETED, async ({ character }) => {
     $('#character_world').val('');
-    await deleteLorebook(character.data.character_book.name);
+    if (character.data?.character_book?.name) {
+      await deleteLorebook(character.data.character_book.name);
+    }
   });
 });
