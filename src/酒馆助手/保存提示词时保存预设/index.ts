a3eb1f9fbdc81602771b09b45086e587f2631614
StageDog <aksanajw845@gmail.com>
2026-02-08 08:29:10 +0800

    fix

--- src/酒馆助手/保存提示词时保存预设/index.ts
+++ src/酒馆助手/保存提示词时保存预设/index.ts
@@ -1,12 +1,6 @@
 import { loadReadme } from '@util/script';
-import { compare } from 'compare-versions';
 
 $(async () => {
-  // 酒馆助手 4.7.0+ 已经将本脚本作为了体验优化选项, 则本脚本不生效, 直接返回
-  if (compare(await getTavernHelperVersion(), '4.7.0', '>=')) {
-    return;
-  }
-
   loadReadme('https://testingcf.jsdelivr.net/gh/StageDog/tavern_resource/src/酒馆助手/保存提示词时保存预设/README.md');
 
   const handler = () => $('#update_oai_preset').trigger('click');
