a3f21ef38b6415305d05973896c7a4d896bd3a71
StageDog <aksanajw845@gmail.com>
2026-02-08 08:32:45 +0800

    fix

--- src/酒馆助手/删除角色卡时删除绑定的主要世界书/index.ts
+++ src/酒馆助手/删除角色卡时删除绑定的主要世界书/index.ts
@@ -1,12 +1,6 @@
 import { loadReadme } from '@util/script';
-import { compare } from 'compare-versions';
 
 $(async () => {
-  // 酒馆助手 4.7.0+ 已经将本脚本作为了体验优化选项, 则本脚本不生效, 直接返回
-  if (compare(await getTavernHelperVersion(), '4.7.0', '>=')) {
-    return;
-  }
-
   loadReadme(
     'https://testingcf.jsdelivr.net/gh/StageDog/tavern_resource/src/酒馆助手/删除角色卡时删除绑定的主要世界书/README.md',
   );
