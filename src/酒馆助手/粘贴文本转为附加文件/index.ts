ba183583d8baf8ac4790812e691b42384acf954d
StageDog <aksanajw845@gmail.com>
2026-02-28 01:13:02 +0800

    [酒馆助手/粘贴文本转为附加文件] fix

--- src/酒馆助手/粘贴文本转为附加文件/index.ts
+++ src/酒馆助手/粘贴文本转为附加文件/index.ts
@@ -14,14 +14,55 @@ function get_settings() {
   return settings;
 }
 
+function human_file_size(bytes: number, si = false, dp = 1) {
+  const thresh = si ? 1000 : 1024;
+
+  if (Math.abs(bytes) < thresh) {
+    return bytes + ' B';
+  }
+
+  const units = si
+    ? ['kB', 'MB', 'GB', 'TB', 'PB', 'EB', 'ZB', 'YB']
+    : ['KiB', 'MiB', 'GiB', 'TiB', 'PiB', 'EiB', 'ZiB', 'YiB'];
+  let u = -1;
+  const r = 10 ** dp;
+
+  do {
+    bytes /= thresh;
+    ++u;
+  } while (Math.round(Math.abs(bytes) * r) / r >= thresh && u < units.length - 1);
+
+  return bytes.toFixed(dp) + ' ' + units[u];
+}
+
+async function on_file_attach(file_list: FileList) {
+  if (!file_list || file_list.length === 0) {
+    return;
+  }
+
+  const name = file_list.length === 1 ? file_list[0].name : SillyTavern.t`${file_list.length} files selected`;
+  const size = [...file_list].reduce((acc, file) => acc + file.size, 0);
+  const title = [...file_list].map(x => x.name).join('\n');
+  $('#file_form .file_name').text(name).attr('title', title);
+  $('#file_form .file_size').text(human_file_size(size)).attr('title', size);
+  $('#file_form').removeClass('displayNone');
+
+  const currentChatId = SillyTavern.getCurrentChatId();
+  if (currentChatId) {
+    eventOnce(tavern_events.CHAT_CHANGED, () => {
+      $('#file_form').trigger('reset');
+    });
+  }
+}
+
 function attach_file_from_text(text: string) {
-  const file = new File([text], 'text.txt');
+  const file = new File([text], 'pasted.txt');
 
   const data_transfer = new DataTransfer();
   data_transfer.items.add(file);
-  const $file_form_input = $('#file_form_input');
-  $file_form_input.prop('files', data_transfer.files);
-  $file_form_input[0].dispatchEvent(new Event('change'));
+
+  $('#file_form_input').prop('files', data_transfer.files);
+  on_file_attach(data_transfer.files);
 }
 
 function on_beforeinput(event: JQuery.TriggeredEvent) {
@@ -37,10 +78,12 @@ function on_beforeinput(event: JQuery.TriggeredEvent) {
   }
 }
 
-const text_area = $('#send_textarea').first();
 $(() => {
+  const text_area = $('#send_textarea').first();
+
   text_area.on('beforeinput', on_beforeinput);
-});
-$(window).on('pagehide', () => {
-  text_area.on('beforeinput', on_beforeinput);
+
+  $(window).on('pagehide', () => {
+    text_area.off('beforeinput', on_beforeinput);
+  });
 });
